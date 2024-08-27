febf8d3091d9affea78a991dbba700817e98c9e6
MSI\PC BAO THONG <baothong15082003@gmail.com>
2024-08-27 22:54:13 +0700

    Tuan01 Developer Skills & Editor Setup Bai01 JS logs a string temperatures + image

--- Tuan01/Developer Skills & Editor Setup/Bai01_Developer Skills & Editor Setup.js
+++ Tuan01/Developer Skills & Editor Setup/Bai01_Developer Skills & Editor Setup.js
@@ -1,3 +1,14 @@
 // MSSV: 21014091
 // Ho va ten: Dang Bao Thong
 // Description: Coding Challenge #2
+function printForecast(arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}°C in ${i + 1} days... `;
+    }
+    console.log('...' + str);
+}
+const arr1 = [17, 21, 23];
+printForecast(arr1);
+const arr2 = [12, 5, -5, 0, 4];
+printForecast(arr2);
