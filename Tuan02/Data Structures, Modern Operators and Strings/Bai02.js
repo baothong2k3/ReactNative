cd11950b7890009978a535645cd1a05cd63e841e
MSI\PC BAO THONG <baothong15082003@gmail.com>
2024-09-10 23:42:45 +0700

    Tuan02 Bai02 Data Structures, Modern Operators and Strings + image

---
+++ Tuan02/Data Structures, Modern Operators and Strings/Bai02.js
@@ -0,0 +1,62 @@
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+for (const [index, player] of game.scored.entries()) {
+    console.log(`Goal ${index + 1}: ${player}`);
+}
+// 2. Use a loop to calculate the average odd and log it to the console
+let average = 0;
+for (const odd of Object.values(game.odds)) {
+    average += odd;
+}
+average /= Object.values(game.odds).length;
+console.log(average);
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25 Odd of victory Borrussia Dortmund: 6.5
+for (const [team, odd] of Object.entries(game.odds)) {
+    console.log(`Odd of victory ${game[team] ?? 'draw'}: ${odd}`);
+}
+// 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this: {Gnarby: 1, Hummels: 1, Lewandowski: 2}
+const scorers = {};
+for (const player of game.scored) {
+    scorers[player] = (scorers[player] ?? 0) + 1;
+}
+console.log(scorers);
