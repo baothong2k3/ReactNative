b29e6ab5212013ab493d63ad26a941811c4eb5cf
MSI\PC BAO THONG <baothong15082003@gmail.com>
2024-10-26 17:01:01 +0700

    Tuan04 lab 04 a + minh chung

---
+++ Tuan02/Data Structures, Modern Operators and Strings/Bai04.js
@@ -0,0 +1,2 @@
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
